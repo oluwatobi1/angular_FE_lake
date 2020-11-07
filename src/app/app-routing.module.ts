782d3c737056b17b2e20dd2808f6ec16f31479e5
oluwatobi1 <akintunlesetobi@gmail.com>
2020-11-07 20:12:06 +0100

    updated team member page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { CovidImpactComponent } from './covid-impact/covid-impact.component';
 import { CtscanComponent } from './ctscan/ctscan.component';
+import { DatasourceComponent } from './datasource/datasource.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectoverviewComponent } from './projectoverview/projectoverview.component';
+import { TeamMembersComponent } from './team-members/team-members.component';
 
 const routes: Routes = [
-  {path:'images', component:CtscanComponent},
-  {path: "", component:HomeComponent}
+  {path:'Images', component:CtscanComponent},
+  {path: "", component:HomeComponent},
+  {path: "Analytics", component:AnalyticsComponent},
+  {path: "Covid-Impact", component:CovidImpactComponent},
+  {path: "Data-Sources", component:DatasourceComponent},
+  {path: "Project-Overview", component:ProjectoverviewComponent},
+  {path: "Team-Members", component:TeamMembersComponent}
 ];
 
 @NgModule({
