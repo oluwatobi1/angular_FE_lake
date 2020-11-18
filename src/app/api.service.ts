2f2557e265dfe1dba095215a333a11a1229e1e03
oluwatobi1 <akintunlesetobi@gmail.com>
2020-11-18 12:50:34 +0100

    changed endpoint

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,8 +8,7 @@ import { Observable } from 'rxjs';
 export class ApiService {
     // https://covidreisparbe.herokuapp.com/route/image/ to be replaced to connect to the BE
     // http://127.0.0.1:8000/route/image/ for local server
-    // http://13.58.198.240:8000/
-  url = "http://13.58.198.240:8000/route/image/"
+  url = "http://3.17.67.158:8000/route/image/"
   
   httpHeaders = new HttpHeaders({'Content-Type':"application/json"})
 
